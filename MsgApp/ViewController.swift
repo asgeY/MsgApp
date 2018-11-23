2e4ff1f96c82cbdd1d0e2097849c83e5f8503660
Asgedom Yohannes <43711479+asgeY@users.noreply.github.com>
2018-11-23 15:49:37 -0600

    Initial Commit

---
+++ MsgApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MsgApp
+//
+//  Created by Asgedom Yohannes on 11/23/18.
+//  Copyright © 2018 Asgedom Yohannes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
